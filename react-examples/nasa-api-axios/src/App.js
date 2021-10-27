974515567a1b92f0f0bdeb77484bf7e61c9b8ea7
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-27 22:53:16 +0300

    theme toggle

--- react-examples/nasa-api-axios/src/App.js
+++ react-examples/nasa-api-axios/src/App.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-import Button from "@mui/material/Button";
+import { Button, Switch } from "@mui/material";
 // import { InsertPhotoIcon } from "@mui/icons-material";
 
+import { IconButton } from "@mui/material";
+import { Fingerprint } from "@mui/icons-material";
+import PhotoIcon from "@mui/icons-material/Photo";
 import axios from "axios";
 
 function App() {
   const [cosmos, setCosmos] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [isImage, setImage] = useState(false);
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   useEffect(() => {
     axios(
@@ -25,15 +33,29 @@ function App() {
   }; //TODO:toggle with boolean
 
   return (
-    <div className="App">
+    <div className={checked ? "Dark" : "Light"}>
+      <Switch
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ "aria-label": "controlled" }}
+      />
       {isLoading && <div>Loading...</div>}
       <p>{cosmos.title}</p>
       <p>{cosmos.date}</p>
-      <p>{cosmos.copyright}</p>
       <p>{cosmos.explanation}</p>
       {isImage && <img src={cosmos.url} alt={cosmos.title} width="200" />}
-
-      <Button onClick={openImage}>LALALA</Button>
+      <Button
+        onClick={openImage}
+        variant="contained"
+        size="large"
+        endIcon={<PhotoIcon />}
+      >
+        TOGGLE
+      </Button>
+      <p>{cosmos.copyright}</p>
+      <IconButton aria-label="fingerprint" size="large">
+        <Fingerprint fontSize="inherit" />
+      </IconButton>
     </div>
   );
 }
