03ce4f36da0f5cf62f2f0b260d9a1e631187bb6b
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-11-09 11:57:04 +0300

    packages trying area

---
+++ react-examples/packages-trying-area/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { MekorialButton } from "somesamebuttons";
+import "somesamebuttons/dist/index.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MekorialButton text="MEKORIALBUTTON" type="link" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
